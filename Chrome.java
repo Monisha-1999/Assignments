7e3e3c76d043544ee5c9c4ef3044e0454674abb9
Monisha-1999 <monishasub99@gmail.com>
2024-06-23 20:38:29 +0530

    Add files via upload
    Week1-day1-Assignments

---
+++ Chrome.java
@@ -0,0 +1,22 @@
+package weekendassignments;
+
+public class Chrome {
+
+	public static void main(String[] args)
+	{
+		float version=91.0f;
+		String developer= "Google";
+		boolean isBeta = false;
+		int ReleaseYear=2008;
+		char shortcutKey='C';
+		
+		System.out.println("The version is "+version);
+		System.out.println("The developer is "+developer);
+		System.out.println("The value of the isBeta is "+isBeta);
+		System.out.println("The value of the ReleaseYear is "+ReleaseYear);
+		System.out.println("The value of the shortcutKey is "+shortcutKey);
+	
+
+	}
+
+}
